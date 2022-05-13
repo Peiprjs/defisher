9de97e66d2409e4a3c63130fb3b327e0b73d3c09
Maro <peipr@peiphy.xyz>
2022-05-13 13:42:36 +0200

    yes

---
+++ dl.sh
@@ -0,0 +1,7 @@
+wget https://peiphy.xyz/quickfiles/1.AVI
+wget https://peiphy.xyz/quickfiles/2.AVI
+wget https://peiphy.xyz/quickfiles/3.AVI
+wget https://peiphy.xyz/quickfiles/4.AVI
+wget https://peiphy.xyz/quickfiles/5.AVI
+wget https://peiphy.xyz/quickfiles/6.AVI
+wget https://peiphy.xyz/quickfiles/7.AVI
